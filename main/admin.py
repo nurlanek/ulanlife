ebcf554770f05ed05b544a909232aebffb1eeae7
Nurlanbek <nurlanbek1@gmail.com>
2024-01-22 15:15:57 +0600

    admin update

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Kroy_detail, Kroy, Uchastok, Masterdata, UserUchastok
-from django.utils.safestring import mark_safe
 
-#hhh
+
+#hhh   df
 # Register your models here.
 class UserUchastokAdmin(admin.ModelAdmin):
     list_display = ("user", "uchastok")
